e6560affa851816543967b03d6ec50f93e1d964f
JacobHaake <jhaake18@gmail.com>
2019-07-02 14:30:13 -0400

    Adding OAuth and Passport

---
+++ client/src/Components/OAuth/Google.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Google extends Component
+{
+    
+
+    render()
+    {
+        return (
+            <div>
+                <h1>Logging in with Google </h1>
+                <a class="home-btn" href="/">Home</a>
+            </div>
+        );
+    }
+}
+
+export default Google;
---
+++ client/src/Components/OAuth/Logout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Logout extends Component
+{
+    render()
+    {
+        return (
+            <div>
+                <h1>Logging out</h1>
+                <a class="home-btn" href="/">Home</a>
+            </div>
+        );
+    }
+}
+
+export default Logout;
