d955412954fcad552fe89d66f71a5a86e702970c
Kinglatour <latourbarry01@hotmail.com>
2019-07-09 18:13:22 -0400

    Home Page Additions

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { MDBIcon, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem } from 'mdbreact';
+import React, { Fragment } from "react";
+import { MDBIcon, MDBDropdown, MDBDropdownToggle, MDBDropdownMenu, MDBDropdownItem, MDBFormInline, MDBBtn, MDBContainer, MDBRow, MDBCol } from 'mdbreact';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Navigation from './Navigation';
 import MyEvent from './MyEvent';
 import Event from './Event';
+import Footer from './Footer';
 
 class Home extends React.Component {
   constructor(props) {
@@ -15,20 +16,30 @@ class Home extends React.Component {
     const scrollContainerStyle = { width: "auto", maxHeight: "auto" };
     return (
       <div style={bgNavy}>
-        <div>
-          <MDBDropdown dropright className="cardpadding ml-2">
-            <MDBDropdownToggle color="transparent">
-             <h3 className="text-white">Past Events<MDBIcon icon="sort-amount-down" className="ml-2 mdb-color-text"/></h3>
-            </MDBDropdownToggle>
-            <MDBDropdownMenu>
-              <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
+        <Navigation/>
+        <div className="d-flex justify-content-between">
+          <div>
+            <MDBDropdown dropright className="cardpadding ml-2">
+              <MDBDropdownToggle color="transparent">
+              <h3 className="text-white">My Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" /></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </div>
+          <div>
+          <MDBFormInline className="md-form cardpadding pr-3">
+            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+          <MDBBtn outline color="white" size="sm" type="submit" className="mr-auto">
+            Search
+          </MDBBtn>
+          </MDBFormInline>
+          </div>
       </div>
-      <Navigation/>
       <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
         <div class="card-inline "><h2><MyEvent/></h2></div>
         <div class="card-inline"><h2><MyEvent/></h2></div>
@@ -39,17 +50,17 @@ class Home extends React.Component {
         <div class="card-inline"><h2><MyEvent/></h2></div>
       </div>
       <div>
-          <MDBDropdown dropright className="ml-2">
-            <MDBDropdownToggle color="transparent">
-             <h3 className="text-white">Group Events<MDBIcon icon="sort-amount-down" className="ml-2 mdb-color-text"/></h3>
-            </MDBDropdownToggle>
-            <MDBDropdownMenu>
-              <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
+        <MDBDropdown dropright className="ml-2">
+          <MDBDropdownToggle color="transparent">
+            <h3 className="text-white">Group Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+          </MDBDropdownToggle>
+          <MDBDropdownMenu>
+            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" /></MDBDropdownItem>
+            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
+            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+          </MDBDropdownMenu>
+        </MDBDropdown>
       </div>
       <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
         <div class="card-inline"><h2><Event/></h2></div>
@@ -61,17 +72,17 @@ class Home extends React.Component {
         <div class="card-inline"><h2><Event/></h2></div>
       </div>
       <div>
-          <MDBDropdown dropright className="ml-2">
-            <MDBDropdownToggle color="transparent">
-             <h3 className="text-white">Past Events<MDBIcon icon="sort-amount-down" className="ml-2 mdb-color-text"/></h3>
-            </MDBDropdownToggle>
-            <MDBDropdownMenu>
-              <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-              <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            </MDBDropdownMenu>
-          </MDBDropdown>
+        <MDBDropdown dropright className="ml-2">
+          <MDBDropdownToggle color="transparent">
+            <h3 className="text-white">Past Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+          </MDBDropdownToggle>
+          <MDBDropdownMenu>
+            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
+            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+          </MDBDropdownMenu>
+        </MDBDropdown>
       </div>
       <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
         <div class="card-inline"><h2><MyEvent/></h2></div>
@@ -82,6 +93,7 @@ class Home extends React.Component {
         <div class="card-inline"><h2><MyEvent/></h2></div>
         <div class="card-inline"><h2><MyEvent/></h2></div>
       </div>
+      <Footer/>
       </div>
     );
   }
--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -27,49 +27,29 @@ class Navigation extends React.Component {
           <header>
             <MDBNavbar style={bgNavy} dark expand="md" scrolling fixed="top">
               <MDBNavbarBrand href="/">
-                  <strong>Hello, User</strong>
+                  <strong>Home</strong>
               </MDBNavbarBrand>
               <MDBNavbarToggler onClick={ this.onClick } />
               <MDBCollapse isOpen = { this.state.collapse } navbar>
                 <MDBNavbarNav left>
-                  <MDBNavItem active>
-                      <MDBNavLink to="#">Home</MDBNavLink>
+                  <MDBNavItem>
+                      <MDBNavLink to="#">Friends</MDBNavLink>
                   </MDBNavItem>
                   <MDBNavItem>
-                      <MDBNavLink to="#">Notifications</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                      <MDBNavLink to="#">My Events</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#">Group Events</MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#">Create Event</MDBNavLink>
+                      <MDBNavLink to="#">Groups</MDBNavLink>
                   </MDBNavItem>
                 </MDBNavbarNav>
                 <MDBNavbarNav right>
-                  <MDBFormInline className="md-form mr-auto m-0">
-                    <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-                  <MDBBtn outline color="white" size="sm" type="submit" className="mr-auto">
-                    Search
-                  </MDBBtn>
-                  </MDBFormInline>
                   <MDBNavItem>
                     <MDBDropdown>
                       <MDBDropdownToggle nav caret>
-                        <span><MDBIcon icon="cog" className="mr-1" /></span>
+                        <span>User Name</span>
                       </MDBDropdownToggle>
                       <MDBDropdownMenu>
-                        <MDBDropdownItem href="#!">Action</MDBDropdownItem>
-                        <MDBDropdownItem href="#!">Another Action</MDBDropdownItem>
-                        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                        <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                        <MDBDropdownItem href="#!">Settings<MDBIcon icon="cog" className="mdb-color-text ml-2" /></MDBDropdownItem>
+                        <MDBDropdownItem href="#!">Sign Out<MDBIcon icon="sign-out-alt" className="red-text ml-2" /></MDBDropdownItem>
                       </MDBDropdownMenu>
                     </MDBDropdown>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="#"><MDBIcon icon="sign-out-alt" className="red-text" /></MDBNavLink>
                   </MDBNavItem>
                 </MDBNavbarNav>
               </MDBCollapse>
