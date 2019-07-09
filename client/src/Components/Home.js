6be0b1fdb7bcf79d4d159982d8827056ef50cc14
Kinglatour <latourbarry01@hotmail.com>
2019-07-09 19:21:51 -0400

    Home Changes

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -17,83 +17,130 @@ class Home extends React.Component {
     return (
       <div style={bgNavy}>
         <Navigation/>
-        <div className="d-flex justify-content-between">
+        <div className="d-flex">
           <div>
             <MDBDropdown dropright className="cardpadding ml-2">
               <MDBDropdownToggle color="transparent">
-              <h3 className="text-white">My Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+                <h3 className="text-white">My Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
               </MDBDropdownToggle>
               <MDBDropdownMenu>
-                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" /></MDBDropdownItem>
-                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-                <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
+                <MDBDropdownItem header>Sort</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
               </MDBDropdownMenu>
             </MDBDropdown>
           </div>
           <div>
-          <MDBFormInline className="md-form cardpadding pr-3">
-            <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-          <MDBBtn outline color="white" size="sm" type="submit" className="mr-auto">
-            Search
-          </MDBBtn>
-          </MDBFormInline>
+            <MDBDropdown dropright className="cardpadding m-0" size="sm">
+              <MDBDropdownToggle color="transparent">
+                <h6 className="text-white"><MDBIcon icon="filter m-0 p-0" className="mdb-color-text"/></h6>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem header>Filter</MDBDropdownItem>
+                <MDBDropdownItem href="#!">High Priority<MDBIcon icon="chevron-circle-up ml-2" className="red-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Medium Priority<MDBIcon icon="chevron-circle-right ml-2" className="orange-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Low Priority<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem header>Event Type</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
           </div>
-      </div>
-      <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
-        <div class="card-inline "><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-      </div>
-      <div>
-        <MDBDropdown dropright className="ml-2">
-          <MDBDropdownToggle color="transparent">
-            <h3 className="text-white">Group Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
-          </MDBDropdownToggle>
-          <MDBDropdownMenu>
-            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" /></MDBDropdownItem>
-            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
-            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-          </MDBDropdownMenu>
-        </MDBDropdown>
-      </div>
-      <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-        <div class="card-inline"><h2><Event/></h2></div>
-      </div>
-      <div>
-        <MDBDropdown dropright className="ml-2">
-          <MDBDropdownToggle color="transparent">
-            <h3 className="text-white">Past Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
-          </MDBDropdownToggle>
-          <MDBDropdownMenu>
-            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
-            <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" /></MDBDropdownItem>
-            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-            <MDBDropdownItem href="#!">Something else here</MDBDropdownItem>
-          </MDBDropdownMenu>
-        </MDBDropdown>
-      </div>
-      <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-        <div class="card-inline"><h2><MyEvent/></h2></div>
-      </div>
-      <Footer/>
+          <div className="ml-auto">
+            <MDBFormInline className="md-form cardpadding pr-3">
+              <input className="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+              <MDBBtn outline color="white" size="sm" type="submit" className="mr-auto">
+                Search
+              </MDBBtn>
+            </MDBFormInline>
+          </div>
+        </div>
+        <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
+          <div class="card-inline "><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+        </div>
+        <div className="d-flex">
+          <div>
+            <MDBDropdown dropright className="ml-2">
+              <MDBDropdownToggle color="transparent">
+                <h3 className="text-white">Group Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem header>Sort</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </div>
+          <div>
+            <MDBDropdown dropright className="m-0" size="sm">
+              <MDBDropdownToggle color="transparent">
+                <h6 className="text-white"><MDBIcon icon="filter m-0 p-0" className="mdb-color-text"/></h6>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem header>Event Type</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </div>
+        </div>
+        <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+          <div class="card-inline"><h2><Event/></h2></div>
+        </div>
+        <div className="d-flex">
+          <div>
+            <MDBDropdown dropright className="ml-2">
+              <MDBDropdownToggle color="transparent">
+                <h3 className="text-white">Past Events<MDBIcon icon="sort-amount-down ml-2" className="ml-2 mdb-color-text"/></h3>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                  <MDBDropdownItem header>Sort</MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Time<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-up ml-2" className="green-text"/></MDBDropdownItem>
+                  <MDBDropdownItem href="#!">Location<MDBIcon icon="angle-double-down ml-2" className="red-text"/></MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </div>
+          <div>
+            <MDBDropdown dropright className="m-0" size="sm">
+              <MDBDropdownToggle color="transparent">
+                <h6 className="text-white"><MDBIcon icon="filter m-0 p-0" className="mdb-color-text"/></h6>
+              </MDBDropdownToggle>
+              <MDBDropdownMenu>
+                <MDBDropdownItem header>Event Type</MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+                <MDBDropdownItem href="#!">Place Holder<MDBIcon icon="chevron-circle-down ml-2" className="green-text"/></MDBDropdownItem>
+              </MDBDropdownMenu>
+            </MDBDropdown>
+          </div>
+        </div>
+        <div className="scrolling-wrapper-flexbox scrollbar scrollbar-primary" style={scrollContainerStyle}>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+          <div class="card-inline"><h2><MyEvent/></h2></div>
+        </div>
+        <Footer/>
       </div>
     );
   }
