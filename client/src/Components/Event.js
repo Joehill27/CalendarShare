9e18f14487d0fe675b895ce0cc3dadb041d43a75
Kinglatour <latourbarry01@hotmail.com>
2019-07-18 13:40:16 -0400

    Modals

--- client/src/Components/Event.js
+++ client/src/Components/Event.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBIcon } from 'mdbreact';
+import { MDBBtn, MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBIcon, MDBModal, MDBModalBody,
+    MDBModalHeader, MDBModalFooter } from 'mdbreact';
 
 class Event extends React.Component {
-    constructor(props) {
-      super(props);
+
+    state = {
+ 
+        modal1: false
+        
+    };
+
+    toggle = nr => () => {
+        let modalNumber = 'modal' + nr
+        this.setState({
+            [modalNumber]: !this.state[modalNumber]
+        });
     }
 
     render() {
@@ -18,7 +29,16 @@ class Event extends React.Component {
                         <MDBCardText><MDBIcon icon="calendar-day" className="mr-2"/>June 7, 2019 9:00 AM - 12:00 PM</MDBCardText>
                         <MDBCardText><MDBIcon icon="users" className="mr-2"/>Event Group</MDBCardText>
                         <MDBCardText><MDBIcon icon="info-circle" className="mr-2"/>Event Summary</MDBCardText>
-                        <MDBBtn size="sm" color="mdb-color darken-2" href="#">View</MDBBtn>
+                        <MDBBtn size="sm" color="mdb-color darken-2" onClick={this.toggle(1)}>View</MDBBtn>
+                            <MDBModal isOpen={this.state.modal1} toggle={this.toggle(1)} centered>
+                                <MDBModalHeader toggle={this.toggle(1)} className="mdb-color darken-2 white-text"></MDBModalHeader>
+                                <MDBModalBody>
+                                    Place Holder
+                                </MDBModalBody>
+                                <MDBModalFooter>
+                                    <MDBBtn color="secondary" onClick={this.toggle(1)}>Close</MDBBtn>
+                                </MDBModalFooter>
+                            </MDBModal>
                         <MDBBtn size="sm" color="green darken-4" href="#">Join</MDBBtn>
                     </MDBCardBody>
                 </MDBCard>
