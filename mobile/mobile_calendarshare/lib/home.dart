07ce2ac1cfcc23008e40ba8ab79903db17becd7e
Joseph Hill <josephmhill96@gmail.com>
2019-07-22 12:47:00 -0400

    Fixed merge conflicts

--- mobile/mobile_calendarshare/lib/home.dart
+++ mobile/mobile_calendarshare/lib/home.dart
@@ -75,21 +75,12 @@ class _HomePageState extends State<HomePage> {
 //  ..add(new Event('Concert', 'Amway Center', 'Biggest s of 2019'))
 //  ..add(new Event('Pool Party', 'Glen\'s Place', 'Summer\'s Hottest Event'));
 
-<<<<<<< HEAD
-  final initialGroupEvents = <Event>[]
-    ..add(new Event('Musical', 'Amway Center', 'Biggest Hits of 2019'))
-    ..add(new Event('Disney Day', 'Magic Kingdom',
-        'Smiths 11th Annual Disney Get-Together'))
-    ..add(new Event('Magics Game', 'Amway Center', 'Biggest hits of 2019'))
-    ..add(new Event('Study PARTY', 'Glen\'s Place', 'Summer\'s Hottest Event'));
-=======
 final initialGroupEvents = <Event>[]
 
   ..add(new Event('Musical', 'Amway Center', 'Biggest Hits of 2019', ))
   ..add(new Event('Disney Day', 'Magic Kingdom', 'Smiths 11th Annual Disney Get-Together', ))
   ..add(new Event('Magics Game', 'Amway Center', 'Biggest hits of 2019', ))
   ..add(new Event('Study PARTY', 'Glen\'s Place', 'Summer\'s Hottest Event', ));
->>>>>>> da1ed9b262145df0b20f0be2817ea1e9a10b23bf
 
   @override
   Widget build(BuildContext context) {
@@ -142,53 +133,6 @@ final initialGroupEvents = <Event>[]
                               ));
                         },
                         child: new Text("Friends"),
-<<<<<<< HEAD
-                      ),
-                    ])),
-                    PopupMenuItem(
-                        child: Row(
-                      children: <Widget>[
-                        Icon(Icons.group),
-                        SizedBox(
-                          width: 7,
-                        ),
-                        GestureDetector(
-                          child: new Text("Groups"),
-                        ),
-                      ],
-                    )),
-                    PopupMenuItem(
-                        child: Row(
-                      children: <Widget>[
-                        Icon(Icons.search),
-                        SizedBox(
-                          width: 7,
-                        ),
-                        GestureDetector(
-                          child: new Text("Search"),
-                        ),
-                      ],
-                    )),
-                    PopupMenuItem(
-                        child: Row(
-                      children: <Widget>[
-                        Icon(Icons.power_settings_new),
-                        SizedBox(
-                          width: 7,
-                        ),
-                        GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => LoginPage(),
-                                ));
-                          },
-                          child: new Text("Logout"),
-                        ),
-                      ],
-                    )),
-=======
                      ),
                     ]
                   )
@@ -243,7 +187,6 @@ final initialGroupEvents = <Event>[]
                       }, 
                       child: new Text("Logout"),
                     ),
->>>>>>> da1ed9b262145df0b20f0be2817ea1e9a10b23bf
                   ],
                 )
               ],
