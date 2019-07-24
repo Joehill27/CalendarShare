74f9e2b99281925aea7912b2672bcbe31c8ef4c3
Joseph Hill <josephmhill96@gmail.com>
2019-07-24 17:17:21 -0400

    Fixed name bug

--- mobile/mobile_calendarshare/lib/home.dart
+++ mobile/mobile_calendarshare/lib/home.dart
@@ -128,7 +128,7 @@ class _HomePageState extends State<HomePage> {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => FriendPage(),
+                          builder: (context) => FriendsListPage(),
                         ));
                   },
                   leading: Icon(Icons.person),
