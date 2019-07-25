d87a1bedb54e8b68f087e89beadb60690e4783e5
migdero13 <migdero@hotmail.com>
2019-07-25 14:53:56 -0400

    mobile edit

--- mobile/mobile_calendarshare/lib/main.dart
+++ mobile/mobile_calendarshare/lib/main.dart
@@ -24,75 +24,20 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      backgroundColor: Colors.blueGrey[900],
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new Stack(
-              alignment: Alignment.topCenter,
-              children: <Widget>[
-                new Container(
-                  margin: new EdgeInsets.only(right: 40.0, top: 0.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF18D191)),
-                  child: new Icon(
-                    Icons.music_note,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(right: 90.0, top: 40.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFC6A7F)),
-                  child: new Icon(
-                    Icons.restaurant,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 0.0, top: 40.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFFFFCE56)),
-                  child: new Icon(
-                    Icons.local_play,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 50.0, top: 0.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Color(0xFF45E0EC)),
-                  child: new Icon(
-                    Icons.school,
-                    color: Colors.white,
-                  ),
-                ),
-                new Container(
-                  margin: new EdgeInsets.only(left: 100.0, top: 40.0),
-                  height: 60.0,
-                  width: 60.0,
-                  decoration: new BoxDecoration(
-                      borderRadius: new BorderRadius.circular(50.0),
-                      color: Colors.deepPurple),
-                  child: new Icon(
-                    Icons.local_airport,
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
+            CircleAvatar( 
+          backgroundImage: AssetImage('assets/images/calendarlogo.png'),
+          minRadius: 20,
+          maxRadius: 100,
+         ),
+
+         SizedBox(
+           height: 20,
+         ),
             new Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -100,7 +45,7 @@ class MyHomePage extends StatelessWidget {
                   padding: const EdgeInsets.only(top: 8.0, bottom: 80.0),
                   child: new Text(
                     "Calendar Share",
-                    style: new TextStyle(fontSize: 30.0),
+                    style: new TextStyle(fontSize: 30.0, color: Colors.white),
                   ),
                 )
               ],
@@ -132,41 +77,7 @@ class MyHomePage extends StatelessWidget {
                 )
               ],
             ),
-            new Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 20.0, right: 5.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFF4364A1),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("FaceBook",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                        left: 10.0, right: 20.0, top: 10.0),
-                    child: new Container(
-                        alignment: Alignment.center,
-                        height: 60.0,
-                        decoration: new BoxDecoration(
-                            color: Color(0xFFDF513B),
-                            borderRadius: new BorderRadius.circular(9.0)),
-                        child: new Text("Google",
-                            style: new TextStyle(
-                                fontSize: 20.0, color: Colors.white))),
-                  ),
-                )
-              ],
-            )
+            
           ],
         ),
       ),
